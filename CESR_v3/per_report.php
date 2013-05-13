031fabcd93eba5454d93f0763e99056a4599b370
Jonathan Medina <jom901@gmail.com>
2013-05-13 10:50:32 -0400

    Static Worker ID is bad

--- CESR_v3/per_report.php
+++ CESR_v3/per_report.php
@@ -16,16 +16,17 @@ if(mysqli_connect_errno($con)){
 echo "<br/>";
 
 
-print $catch = $_POST['date'];
+$catch = $_POST['date'];
 
 $pieces = explode("-", $catch);
 
+$catch2 = $_POST['w_id']
 $result= mysqli_query($con, "SELECT Pr.p_name, SUM(hours) AS time
 FROM Worktime AS WT
 INNER JOIN Project AS Pr ON WT.p_id = Pr.p_id
 INNER JOIN Phase AS Ph ON WT.ph_id = Ph.ph_id
 INNER JOIN Task AS T ON WT.t_id = T.t_id
-WHERE WT.w_id = 7 and MONTH(WT.date) = '$pieces[1]' and YEAR(WT.date) = '$pieces[0]'
+WHERE WT.w_id = '$catch2' and MONTH(WT.date) = '$pieces[1]' and YEAR(WT.date) = '$pieces[0]'
 group by Pr.p_name");
 
 while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
@@ -42,7 +43,7 @@ FROM Worktime AS WT
 INNER JOIN Project AS Pr ON WT.p_id = Pr.p_id
 INNER JOIN Phase AS Ph ON WT.ph_id = Ph.ph_id
 INNER JOIN Task AS T ON WT.t_id = T.t_id
-WHERE WT.w_id = 7 and MONTH(WT.date) = '$pieces[1]' and YEAR(WT.date) = '$pieces[0]'
+WHERE WT.w_id = '$catch2' and MONTH(WT.date) = '$pieces[1]' and YEAR(WT.date) = '$pieces[0]'
 group by T.taskcode");
 
 while ($row = mysqli_fetch_array($going, MYSQL_ASSOC)) {
